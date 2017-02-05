52a3f1fd1a056776c2206f72119b8ac664ee6632
LambdaXymox <lambda.xymox@gmail.com>
2017-02-05 01:44:03 -0600

    Add more variants of basic vector operations in two dimensions.

--- src/vector.rs
+++ src/vector.rs
@@ -32,6 +32,30 @@ impl<T> ops::Add<Vec2d<T>> for Vec2d<T> where T: ops::Add<T, Output=T> {
     }
 }
 
+impl<'a, T> ops::Add<Vec2d<T>> for &'a Vec2d<T> where T: Copy + ops::Add<T, Output=T> {
+    type Output = Vec2d<T>;
+
+    fn add(self, rhs: Vec2d<T>) -> Vec2d<T> {
+        let x: T = self.x + rhs.x;
+        let y: T = self.y + rhs.y;
+
+        vec2d(x, y)
+    }
+}
+
+impl<'a, 'b, T> ops::Add<&'a Vec2d<T>> for &'b Vec2d<T> 
+    where T: Copy + ops::Add<T, Output=T> 
+{
+    type Output = Vec2d<T>;
+
+    fn add(self, other: &'a Vec2d<T>) -> Vec2d<T> {
+        let x = self.x + other.x;
+        let y = self.y + other.y;
+
+        vec2d(x, y)
+    }
+}
+
 impl<T> ops::Sub for Vec2d<T> where T: ops::Sub<T, Output=T> {
     type Output = Vec2d<T>;
 
@@ -43,7 +67,29 @@ impl<T> ops::Sub for Vec2d<T> where T: ops::Sub<T, Output=T> {
     }
 }
 
+impl<'a, T> ops::Sub<Vec2d<T>> for &'a Vec2d<T> where T: Copy + ops::Sub<T, Output=T> {
+    type Output = Vec2d<T>;
+
+    fn sub(self, rhs: Vec2d<T>) -> Vec2d<T> {
+        let x: T = self.x - rhs.x;
+        let y: T = self.y - rhs.y;
+
+        vec2d(x, y)
+    }
+}
+
 impl<T> ops::Mul<T> for Vec2d<T> where T: Copy + ops::Mul<T, Output=T> {
+    type Output = Vec2d<T>;
+
+    fn mul(self, other: T) -> Vec2d<T> {
+        let x = self.x * other;
+        let y = self.y * other;
+
+        vec2d(x, y)
+    }
+}
+
+impl<'a, T> ops::Mul<T> for &'a Vec2d<T> where T: Copy + ops::Mul<T, Output=T> {
     type Output = Vec2d<T>;
 
     fn mul(self, other: T) -> Vec2d<T> {
@@ -65,7 +111,25 @@ impl<T> ops::Div<T> for Vec2d<T> where T: Copy + ops::Div<T, Output=T> {
     }
 }
 
+impl<'a, T> ops::Div<T> for &'a Vec2d<T> where T: Copy + ops::Div<T, Output=T> {
+    type Output = Vec2d<T>;
+
+    fn div(self, other: T) -> Vec2d<T> {
+        let x = self.x / other;
+        let y = self.y / other;
+
+        vec2d(x, y)
+    }
+}
+
 impl<T> ops::AddAssign<Vec2d<T>> for Vec2d<T> where T: Copy + ops::Add<T, Output=T> {
+    fn add_assign(&mut self, rhs: Vec2d<T>) {
+        self.x = self.x + rhs.x;
+        self.y = self.y + rhs.y;
+    }
+}
+
+impl<'a, T> ops::AddAssign<Vec2d<T>> for &'a mut Vec2d<T> where T: Copy + ops::Add<T, Output=T> {
     fn add_assign(&mut self, rhs: Vec2d<T>) {
         self.x = self.x + rhs.x;
         self.y = self.y + rhs.y;
@@ -79,6 +143,13 @@ impl<T> ops::SubAssign<Vec2d<T>> for Vec2d<T> where T: Copy + ops::Sub<T, Output
     }
 }
 
+impl<'a, T> ops::SubAssign<Vec2d<T>> for &'a mut Vec2d<T> where T: Copy + ops::Sub<T, Output=T> {
+    fn sub_assign(&mut self, rhs: Vec2d<T>) {
+        self.x = self.x - rhs.x;
+        self.y = self.y - rhs.y;
+    }
+}
+
 impl<T> ops::MulAssign<T> for Vec2d<T> where T: Copy + ops::Mul<T, Output=T> {
     fn mul_assign(&mut self, rhs: T) {
         self.x = self.x * rhs;
@@ -86,7 +157,21 @@ impl<T> ops::MulAssign<T> for Vec2d<T> where T: Copy + ops::Mul<T, Output=T> {
     }
 }
 
+impl<'a, T> ops::MulAssign<T> for &'a mut Vec2d<T> where T: Copy + ops::Mul<T, Output=T> {
+    fn mul_assign(&mut self, rhs: T) {
+        self.x = self.x * rhs;
+        self.y = self.y * rhs;
+    }
+}
+
 impl<T> ops::DivAssign<T> for Vec2d<T> where T: Copy + ops::Div<T, Output=T> {
+    fn div_assign(&mut self, rhs: T) {
+        self.x = self.x / rhs;
+        self.y = self.y / rhs;
+    }
+}
+
+impl<'a, T> ops::DivAssign<T> for &'a mut Vec2d<T> where T: Copy + ops::Div<T, Output=T> {
     fn div_assign(&mut self, rhs: T) {
         self.x = self.x / rhs;
         self.y = self.y / rhs;
