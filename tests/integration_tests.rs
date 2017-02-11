d64af7a9382ab2dfa275bcfbad3b4a6b402d9129
LambdaXymox <lambda.xymox@gmail.com>
2017-02-11 04:28:15 -0600

    Amend integration test to show that the z buffer actually did something.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -104,4 +104,7 @@ fn test_z_buffer_should_not_affect_rendering_with_one_primitive() {
     // Since we are only rendering one triangle primitive, the frame buffers should
     // agree with each other.
     assert_eq!(frame_buffer_z, frame_buffer_no_z);
+    // The Z-buffering code actually wrote something in.
+    let zero = frame_buffer::frame_buffer(width, height);
+    assert_ne!(frame_buffer_z, zero);
 }
