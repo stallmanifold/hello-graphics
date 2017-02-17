6bcd47f180542fc9100ab8baa516c59cea62ef58
LambdaXymox <lambda.xymox@gmail.com>
2017-02-17 03:15:29 -0600

    Add unit test for Gouraud shader.

--- src/shader/gouraud.rs
+++ src/shader/gouraud.rs
@@ -25,5 +25,33 @@ fn_impl!(Gouraud, N, (Vector3<N>, Vector3<N>, Vector3<N>, Point3<N>));
 
 #[cfg(test)]
 mod tests {
+    use nalgebra::{BaseFloat, ApproxEq, Vector3, Point3};
+     
 
+    /// 
+    /// Compute the gouraud shading of a triangle primitive.
+    ///
+    pub fn gouraud<N>(color0: Vector3<N>, 
+                      color1: Vector3<N>, 
+                      color2: Vector3<N>, 
+                      bary:   Point3<N>) -> Vector3<N>
+        where N: BaseFloat
+    {
+        color0 * bary[0] + color1 * bary[1] + color2 * bary[2]
+    }
+
+    #[test]
+    fn test_gouraud_map_should_yield_same_results_as_function() {
+        let shader = super::shader();
+
+        let color0 = Vector3::new(1.0, 0.0, 0.0);
+        let color1 = Vector3::new(0.0, 1.0, 0.0);
+        let color2 = Vector3::new(0.0, 0.0, 1.0);
+        let bary   = Point3::new(0.2, 0.5, 0.3);
+
+        let res = shader(color0, color1, color2, bary);
+        let ans = gouraud(color0, color1, color2, bary);
+
+        assert!(res.approx_eq(&ans));
+    }
 }
