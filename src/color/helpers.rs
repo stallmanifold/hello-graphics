d5197b3fe12b06101640970afe64bbe44b410ebb
LambdaXymox <lambda.xymox@gmail.com>
2017-02-19 18:36:40 -0600

    Define RgbCast implementation for Point3 as well.

--- src/color/helpers.rs
+++ src/color/helpers.rs
@@ -1,5 +1,6 @@
 use color::rgb::Rgb;
 use nalgebra::Vector3;
+use nalgebra::Point3;
 use num_traits::Float;
 use alga::general::Real;
 
@@ -8,17 +9,30 @@ use alga::general::Real;
 /// Calculate the RGB color of a color vector.
 ///
 #[inline(always)]
-pub fn rgb<N: Float + Real, R: RgbCast<N>>(color: Vector3<N>) -> R {
+pub fn rgb<V, R: RgbCast<V, RgbValue=R>>(color: V) -> R {
     R::rgb_cast(color)
 }
 
-pub trait RgbCast<N> where N: Float + Real {
+pub trait RgbCast<V> {
     type RgbValue;
 
-    fn rgb_cast(color: Vector3<N>) -> Self::RgbValue;
+    fn rgb_cast(color: V) -> Self::RgbValue;
 }
 
-impl RgbCast<f32> for Rgb {
+/*
+macro_rules! rgb_cast_impl {
+    ($float_type: ty) => {
+        impl RgbCast<> for Rgb {
+            type RgbValue = Rgb;
+
+            #[inline]
+            fn rgb_cast(color: )
+        }
+    }
+}
+*/
+
+impl RgbCast<Vector3<f32>> for Rgb {
     type RgbValue = Rgb;
 
     #[inline]
@@ -30,8 +44,8 @@ impl RgbCast<f32> for Rgb {
         Rgb::from_channels(r, g, b)
     }
 }
-/*
-impl RgbCast<f32> for Rgb {
+
+impl<'a> RgbCast<&'a Vector3<f32>> for Rgb {
     type RgbValue = Rgb;
 
     #[inline]
@@ -43,8 +57,8 @@ impl RgbCast<f32> for Rgb {
         Rgb::from_channels(r, g, b)
     }
 }
-*/
-impl RgbCast<f64> for Rgb {
+
+impl RgbCast<Vector3<f64>> for Rgb {
     type RgbValue = Rgb;
 
     #[inline]
@@ -56,8 +70,8 @@ impl RgbCast<f64> for Rgb {
         Rgb::from_channels(r, g, b)
     }
 }
-/*
-impl RgbCast<f64> for Rgb {
+
+impl<'a> RgbCast<&'a Vector3<f64>> for Rgb {
     type RgbValue = Rgb;
 
     #[inline]
@@ -69,4 +83,55 @@ impl RgbCast<f64> for Rgb {
         Rgb::from_channels(r, g, b)
     }
 }
-*/
+
+impl RgbCast<Point3<f32>> for Rgb {
+    type RgbValue = Rgb;
+
+    #[inline]
+    fn rgb_cast(color: Point3<f32>) -> Self::RgbValue {
+        let r: u8 = Real::trunc(255.0 * color.x) as u8;
+        let g: u8 = Real::trunc(255.0 * color.y) as u8;
+        let b: u8 = Real::trunc(255.0 * color.z) as u8;
+
+        Rgb::from_channels(r, g, b)
+    }
+}
+
+impl<'a> RgbCast<&'a Point3<f32>> for Rgb {
+    type RgbValue = Rgb;
+
+    #[inline]
+    fn rgb_cast(color: &Point3<f32>) -> Self::RgbValue {
+        let r: u8 = Real::trunc(255.0 * color.x) as u8;
+        let g: u8 = Real::trunc(255.0 * color.y) as u8;
+        let b: u8 = Real::trunc(255.0 * color.z) as u8;
+
+        Rgb::from_channels(r, g, b)
+    }
+}
+
+impl RgbCast<Point3<f64>> for Rgb {
+    type RgbValue = Rgb;
+
+    #[inline]
+    fn rgb_cast(color: Point3<f64>) -> Self::RgbValue {
+        let r: u8 = Real::trunc(255.0 * color.x) as u8;
+        let g: u8 = Real::trunc(255.0 * color.y) as u8;
+        let b: u8 = Real::trunc(255.0 * color.z) as u8;
+
+        Rgb::from_channels(r, g, b)
+    }
+}
+
+impl<'a> RgbCast<&'a Point3<f64>> for Rgb {
+    type RgbValue = Rgb;
+
+    #[inline]
+    fn rgb_cast(color: &Point3<f64>) -> Self::RgbValue {
+        let r: u8 = Real::trunc(255.0 * color.x) as u8;
+        let g: u8 = Real::trunc(255.0 * color.y) as u8;
+        let b: u8 = Real::trunc(255.0 * color.z) as u8;
+
+        Rgb::from_channels(r, g, b)
+    }
+}
